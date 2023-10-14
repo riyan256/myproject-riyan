829ca55dc6c46facc8661b298043607f22900849
riyan256 <thelikethisriyan@gmail.com>
2023-10-14 16:41:53 +0700

    My Final Project Hactive8 Prakerja Batch 16

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-// Minimal 10 line
-alert("Welcome to My Project")
+// Memberi warna text homepage-content
+const blink1 = document.getElementById("text1");
+const blink2 = document.getElementById("text2");
+const blink3 = document.getElementById("text3");
+
+function Color(element, color) {
+    const currentColor = element.style.color;
+    element.style.color = currentColor === color ? "black" : color;
+}
+
+setInterval(() => Color(blink1, "red"), 500);
+setInterval(() => Color(blink2, "green"), 500);
+setInterval(() => Color(blink3, "blue"), 500);
+
+setTimeout(() => {clearInterval(blink1);}, 1000);
+setTimeout(() => {clearInterval(blink2);}, 1000);
+setTimeout(() => {clearInterval(blink3);}, 1000);
